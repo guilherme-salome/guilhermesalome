82c9696e2cfb0057570a17c787a0687e61e7e919
Guilherme Salome <guilhermesalome@gmail.com>
2017-07-28 14:06:37 -0300

    Finished new index page

--- script/index.js
+++ script/index.js
@@ -5,3 +5,16 @@ window.onload = function() {
     return false;
   })
 }
+
+$(document).ready(function() {
+  var movementStrength = 20;
+  var height = movementStrength / $(window).height();
+  var width = movementStrength / $(window).width();
+  $("#hello").mousemove(function(e){
+    var pageX = e.pageX - ($(window).width() / 2);
+    var pageY = e.pageY - ($(window).height() / 2);
+    var newvalueX = width * pageX;
+    var newvalueY = height * pageY;
+    $('#hello').css("background-position", newvalueX+"px     "+newvalueY+"px");
+  });
+});
