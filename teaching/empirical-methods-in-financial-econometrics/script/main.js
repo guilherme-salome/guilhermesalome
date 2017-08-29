bbf088a9b4584761e8011513d0d2dbc51a1f333e
Guilherme Salome <guilhermesalome@gmail.com>
2017-08-29 17:24:25 -0400

    Added a new post in the pdf format. Extended javascript to distinct html files from pdf files when linking the user.

--- teaching/empirical-methods-in-financial-econometrics/script/main.js
+++ teaching/empirical-methods-in-financial-econometrics/script/main.js
@@ -2,7 +2,13 @@ window.onload = function() {
   var courseName = $("#hi-message").html().split(' ').join('-').toLowerCase();
   $(".blog-post").click(function(e) {
     var postName = $(this).find(".title").html().split(' ').join('-').toLowerCase();
-    var postLink = "http://www.guilhermesalome.com/teaching/" + courseName + '/posts/' + postName + '.html';
+    var isPDF = $(this).hasClass("pdf");
+    var postLink = "http://www.guilhermesalome.com/teaching/" + courseName + '/posts/' + postName;
+    if(isPDF) {
+      postLink = postLink + '.pdf';
+    } else {
+      postLink = postLink + '.html';
+    }
     window.location.replace(postLink);
     return false;
   })
