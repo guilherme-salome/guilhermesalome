c2fa5c08531abe734a9268485f5dff72ecd6fc16
Guilherme <gfs8@duke.edu>
2020-02-04 16:21:18 -0500

    Research now displays paper summaries and correctly loads pdfs.

--- script/research.js
+++ script/research.js
@@ -1,16 +1,26 @@
 document.addEventListener('click', function (event) {
     if (event.srcElement.id === 'home-button' || event.target.matches('.fa-home')) {
-	// Home button
-	window.location.href = '/index.html';
+        // Home button
+        window.location.href = '/index.html';
     } else if (event.srcElement.id === 'back-button'
-	       || event.target.matches('.fa-arrow-left')) {
-	// Back button
-	address = window.location.href;
-	window.location.href = address.slice(0, address.lastIndexOf('/'));
-    } else if (event.target.matches('.paper-title') || event.target.matches('.paper')) {
-	// Paper
-	filename = parsePaperTitle(event.target.innerText)
-	window.location.href = './research/' + filename + '.pdf';
+               || event.target.matches('.fa-arrow-left')) {
+        // Back button
+        address = window.location.href;
+        window.location.href = address.slice(0, address.lastIndexOf('/'));
+    } else if (event.target.matches('.paper-title')) {
+        // div containing only paper title
+        filename = parsePaperTitle(event.target.innerText)
+        window.location.href = './research/' + filename + '.pdf';
+    } else if (event.target.matches('.summary')) {
+        // sibling div that has summary text
+        title = event.srcElement.parentNode.children[0].innerText;
+        filename = parsePaperTitle(title);
+        window.location.href = './research/' + filename + '.pdf';
+    } else if (event.target.matches('.paper')) {
+        // parent div
+        title = event.srcElement.children[0].innerText;
+        filename = parsePaperTitle(title);
+        window.location.href = './research/' + filename + '.pdf';
     }
 });
 
